52c35622a20f064d7061115fc970a808e35a2e13
Seongbin Kim <seongbin9786@gmail.com>
2023-03-08 17:57:41 +0900

    feat(Travel List Page): Changed container style to grid

--- src/components/TravelList.tsx
+++ src/components/TravelList.tsx
@@ -1,3 +1,4 @@
+import { SimpleGrid } from '@chakra-ui/react';
 import { TravelItemType } from '../api/TravelListApi';
 import { TravelListItem } from './TravelListItem';
 
@@ -9,10 +10,19 @@ export function TravelList({ listFetcher }: TravelListProps) {
   const list = listFetcher();
 
   return (
-    <div>
+    <SimpleGrid
+      columns={{
+        sm: 1,
+        xl: 2,
+        '3xl': 3,
+      }}
+      spacingX={16}
+      spacingY={8}
+      padding={32}
+    >
       {list.map((item) => (
         <TravelListItem key={item.idx} item={item} />
       ))}
-    </div>
+    </SimpleGrid>
   );
 }
